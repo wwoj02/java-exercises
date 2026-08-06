b91bdf69fcb697296f7beaaf5a6f4997082d064f
Wojtek <wojtek0201@gmail.com>
2026-08-06 03:40:47 +0200

    access modifiers exercises done 06.08.2026

--- src/main/java/com/amigoscode/_2_developers/_4_accessmodifiers/AccessDemo.java
+++ src/main/java/com/amigoscode/_2_developers/_4_accessmodifiers/AccessDemo.java
@@ -10,19 +10,24 @@ public class AccessDemo {
 
     public static void main(String[] args) {
         // TODO: 1 - Create an instance of AccessModifiers.
+        AccessModifiers accessModifiers = new AccessModifiers();
 
         System.out.println("=== Calling Public Method ===");
         // TODO: 2 - Call the publicGreeting() method on your instance and print the result.
-        //  This works because public methods are accessible from anywhere.
+        //  This works because public methods are accessible from anywhere
+        System.out.println(accessModifiers.publicGreeting());
 
         System.out.println("\n=== Calling Package-Private Method ===");
         // TODO: 3 - Call the packageGreeting() method on your instance and print the result.
         //  This works because AccessDemo is in the SAME package as AccessModifiers.
+        System.out.println(accessModifiers.packageGreeting());
 
         System.out.println("\n=== Accessing Private Field Through Getter ===");
         // TODO: 4 - Use setSecret() to set a value, then use getSecret() to read it back.
         //  Print the result. This demonstrates proper encapsulation:
         //  we access the private field only through public getter/setter methods.
+        accessModifiers.setSecret("12345");
+        System.out.println(accessModifiers.getSecret());
 
         System.out.println("\n=== Why Direct Private Access Won't Work ===");
         // TODO: 5 - Write a comment explaining why the following line would NOT compile:
@@ -30,5 +35,8 @@ public class AccessDemo {
         //  Then leave the line commented out, preceded by your explanation.
         //  Hint: think about what 'private' means for field visibility outside the class.
 
+//        well it's not gonna work cause an attribute with private access modifier can be accessed or override only
+//        from inside the class or using a public methods such as a getter/setter if it were implemented
+//        accessModifiers.secret = "trying direct access";
     }
 }
