9930042ad044926936e2bc72e5f61c990f6acaa6
Wojtek <wojtek0201@gmail.com>
2026-08-19 02:03:33 +0200

    linkedlist exercises done

--- src/main/java/com/amigoscode/_4_datastructures/_5_linkedlists/LinkedListExercises.java
+++ src/main/java/com/amigoscode/_4_datastructures/_5_linkedlists/LinkedListExercises.java
@@ -11,32 +11,43 @@ public class LinkedListExercises {
     public static void main(String[] args) {
 
         // TODO: 1 - Create a LinkedList of Strings called 'list'
-
+        LinkedList<String> list = new LinkedList<>();
 
         // TODO: 2 - Add elements at the beginning using addFirst(): "First", "Second"
         //           Print the list after each addition to observe the order
-
+        list.addFirst("first");
+        System.out.println(list);
+        list.addFirst("second");
+        System.out.println(list);
 
         // TODO: 3 - Add elements at the end using addLast(): "Third", "Fourth"
         //           Print the list
-
+        list.addLast("third");
+        list.addLast("fourth");
 
         // TODO: 4 - Get and print the first element using getFirst()
         //           Get and print the last element using getLast()
-
+        System.out.println(list.getFirst());
+        System.out.println(list.getLast());
 
         // TODO: 5 - Remove the first element using removeFirst()
         //           Remove the last element using removeLast()
         //           Print the list after removals
-
+        list.removeFirst();
+        list.removeLast();
+        System.out.println(list);
 
         // TODO: 6 - Add an element at index 1 using add(index, element): "Inserted"
         //           Print the list to see the element in position
-
+        list.add(1, "Inserted");
+        System.out.println(list);
 
         // TODO: 7 - Iterate using an Iterator and print each element
         //           Use list.iterator() to get the Iterator
         //           Use hasNext() and next() in a while loop
-
+        Iterator<String> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 }
--- src/main/java/com/amigoscode/_4_datastructures/_5_linkedlists/LinkedListVsArrayList.java
+++ src/main/java/com/amigoscode/_4_datastructures/_5_linkedlists/LinkedListVsArrayList.java
@@ -13,23 +13,76 @@ public class LinkedListVsArrayList {
 
         // TODO: 1 - Create both an ArrayList<Integer> and a LinkedList<Integer>
         //           Fill each with 10000 elements (0 to 9999) using a for loop
-
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < 10000; i++) {
+            arrayList.add(i);
+            linkedList.add(i);
+        }
 
         // TODO: 2 - Measure time to add an element at the beginning (index 0) for both lists
         //           Use System.nanoTime() before and after the operation
         //           Perform the add(0, value) operation 1000 times for each list
         //           Print the time taken for each in milliseconds
         //           (divide nanoseconds by 1_000_000 to get milliseconds)
+        long start1 = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            arrayList.add(0, 5);
+        }
+        long end1 = System.nanoTime();
 
+        long start2 = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            linkedList.add(0, 5);
+        }
+        long end2 = System.nanoTime();
+
+        long result1 = end1 - start1;
+
+        long result2 = end2 - start2;
+
+        System.out.println(result1 / 1_000_000);
+        System.out.println(result2 / 1_000_000);
 
         // TODO: 3 - Measure time to add an element at the end for both lists
         //           Perform the add(value) operation 1000 times for each list
         //           Print the time taken for each
+        start1 = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            arrayList.add(5);
+        }
+        end1 = System.nanoTime();
+        result1 = end1 - start1;
 
+        start2 = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            linkedList.add(5);
+        }
+        end2 = System.nanoTime();
+        result2 = end2 - start2;
+
+        System.out.println(result1 / 1_000_000);
+        System.out.println(result2 / 1_000_000);
 
         // TODO: 4 - Measure time to get an element at the middle index for both lists
         //           Perform get(list.size() / 2) operation 1000 times for each list
         //           Print the time taken for each
+        start1 = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            arrayList.get(arrayList.size() / 2);
+        }
+        end1 = System.nanoTime();
+        result1 = end1 - start1;
+
+        start2 = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            linkedList.get(linkedList.size() / 2);
+        }
+        end2 = System.nanoTime();
+        result2 = end2 - start2;
+
+        System.out.println(result1 / 1_000_000);
+        System.out.println(result2 / 1_000_000);
 
 
         // TODO: 5 - Print a summary explaining the differences
@@ -37,6 +90,13 @@ public class LinkedListVsArrayList {
         //           - Why LinkedList is faster for insertions at the beginning
         //           - Why ArrayList is faster for random access (get by index)
         //           - When you would choose one over the other
+        System.out.println("" +
+                "- LinkedList is faster for insertion at the beginning cause linkedlist basically sets a new node at the beginning" +
+                " when arraylist gotta move all objects that currently contain one place ahead to make space for a new object at the beginning");
+        System.out.println("" +
+                "- we can access what arraylist stores with the index, linkedlist gotta iterate to demanded index (no direct access with index)");
+        System.out.println("i'd choose arraylist when its important for direct access through the index, on another hand" +
+                "i'd choose linkedlist when access to middle objects isn't that important as access to the object at the end and the beginning");
 
     }
 }
