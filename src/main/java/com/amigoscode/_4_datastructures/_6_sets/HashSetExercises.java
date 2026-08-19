6f7794aa8d0404a6248bb3bff9f89e746fdb3280
Wojtek <wojtek0201@gmail.com>
2026-08-19 02:28:59 +0200

    set/treeset exercises done

--- src/main/java/com/amigoscode/_4_datastructures/_6_sets/HashSetExercises.java
+++ src/main/java/com/amigoscode/_4_datastructures/_6_sets/HashSetExercises.java
@@ -14,27 +14,36 @@ public class HashSetExercises {
     public static void main(String[] args) {
 
         // TODO: 1 - Create a HashSet of Strings called 'fruits'
-
+        Set<String> fruits = new HashSet<>();
 
         // TODO: 2 - Add elements: "Apple", "Banana", "Cherry", "Apple", "Date", "Banana"
         //           Print the set after adding all elements
         //           Observe that duplicates are automatically ignored
-
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+        fruits.add("Apple");
+        fruits.add("Date");
+        fruits.add("Banana");
+        System.out.println(fruits);
 
         // TODO: 3 - Check if "Cherry" exists in the set using contains()
         //           Print the result
-
+        System.out.println(fruits.contains("Cherry"));
 
         // TODO: 4 - Remove "Date" from the set
         //           Print the set after removal
-
+        fruits.remove("Date");
+        System.out.println(fruits);
 
         // TODO: 5 - Get and print the size of the set
-
+        System.out.println(fruits.size());
 
         // TODO: 6 - Iterate over the set using a for-each loop and print each element
         //           Note: the order may not match insertion order (HashSet is unordered)
-
+        for (String s : fruits) {
+            System.out.println(s);
+        }
 
         // --- Removing Duplicates from a List ---
         System.out.println("\n--- Removing Duplicates ---");
@@ -46,6 +55,9 @@ public class HashSetExercises {
         // TODO: 7 - Use a HashSet to remove duplicates from 'listWithDuplicates'
         //           Create a new HashSet from the list, then create a new ArrayList from the set
         //           Print the deduplicated list
+        Set<String> newSet = new HashSet<>(listWithDuplicates);
+        List<String> newList = new ArrayList<>(newSet);
 
+        System.out.println(newList);
     }
 }
--- src/main/java/com/amigoscode/_4_datastructures/_6_sets/TreeSetExercises.java
+++ src/main/java/com/amigoscode/_4_datastructures/_6_sets/TreeSetExercises.java
@@ -3,6 +3,7 @@ package com.amigoscode._4_datastructures._6_sets;
 // Exercise: TreeSet Operations
 // Learn how to use TreeSet - a sorted set implementation backed by a TreeMap.
 
+import java.util.Set;
 import java.util.TreeSet;
 
 public class TreeSetExercises {
@@ -10,26 +11,37 @@ public class TreeSetExercises {
     public static void main(String[] args) {
 
         // TODO: 1 - Create a TreeSet of Integers called 'numbers'
-
+        TreeSet<Integer> numbers = new TreeSet<>();
 
         // TODO: 2 - Add these elements: 50, 20, 40, 10, 30, 60, 15, 45
         //           Print the set and observe that elements are automatically sorted
-
+        numbers.add(50);
+        numbers.add(20);
+        numbers.add(40);
+        numbers.add(10);
+        numbers.add(30);
+        numbers.add(60);
+        numbers.add(15);
+        numbers.add(45);
+        System.out.println(numbers);
 
         // TODO: 3 - Get and print the first (lowest) element using first()
         //           Get and print the last (highest) element using last()
-
+        System.out.println(numbers.first());
+        System.out.println(numbers.last());
 
         // TODO: 4 - Get a subset of elements from 20 (inclusive) to 45 (exclusive) using subSet()
         //           Print the subset
-
+        System.out.println(numbers.subSet(20, 45));
 
         // TODO: 5 - Get and print the headSet (elements less than 30)
         //           Get and print the tailSet (elements greater than or equal to 30)
-
+        System.out.println(numbers.headSet(30));
+        System.out.println(numbers.tailSet(30));
 
         // TODO: 6 - Iterate over the TreeSet using a for-each loop
         //           Print each element and observe the natural ascending order
+        for (Integer i : numbers) System.out.println(i);
 
     }
 }
